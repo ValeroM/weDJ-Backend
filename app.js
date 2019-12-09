5f34ab7ecddcd901481ccfd905a3513e37c81d22
Marco Valero <marcovalero6@gmail.com>
2019-12-09 18:26:54 -0500

    fixed cors policy again

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ https://stackoverflow.com/questions/5027705/error-content-type-is-not-allowed-by
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', 'https://wedj2019.herokuapp.com/');
+  res.header('Access-Control-Allow-Origin', 'https://wedj2019.herokuapp.com');
   // Request methods you wish to allow
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   // Request headers you wish to allow
