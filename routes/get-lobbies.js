829ad53b5aa6b227961001d295355b75ccaabdaa
Marco Valero <marcovalero6@gmail.com>
2019-12-11 12:05:33 -0500

    cors issue with iphone

--- routes/get-lobbies.js
+++ routes/get-lobbies.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const { lobbies } = require("../controllers"); // gets lobbies directory from the index.js under controllers directory
 
 const setup = () => {
   const controller = lobbies(); // imports the controllers (i.e, whatever logic we want to perform)
   const router = express.Router();
 
-  router.options("/", cors());
+  // router.options("/", cors());
   router.get("/", controller.lobbieslist);
-  router.post("/", cors(), controller.addNewLobby);
+  router.post("/", controller.addNewLobby);
 
   return router;
 };
