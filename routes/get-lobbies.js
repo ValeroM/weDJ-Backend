aef99acd5e2d502ec63dcd5b88189d5c2c82d379
Marco Valero <marcovalero6@gmail.com>
2019-12-11 12:00:32 -0500

    trying to fix iphone bug

--- routes/get-lobbies.js
+++ routes/get-lobbies.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const { lobbies } = require("../controllers"); // gets lobbies directory from the index.js under controllers directory
 
 const setup = () => {
   const controller = lobbies(); // imports the controllers (i.e, whatever logic we want to perform)
   const router = express.Router();
 
+  router.options("/", cors());
   router.get("/", controller.lobbieslist);
-  router.post("/", controller.addNewLobby);
+  router.post("/", cors(), controller.addNewLobby);
 
   return router;
 };
--- routes/get-songs.js
+++ routes/get-songs.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// const cors = require("cors");
 const { songs } = require("../controllers"); // the syntax "{ songs }" is the destructuring assignment syntax. This is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects,into distinct variables.
 // in order to be able to use the controllers by importing from the controller directory, the "songs"  variable has to match the key under the index.js file under controllers directory
 
@@ -6,6 +7,7 @@ const setup = () => {
   const controller = songs(); // imports the controllers (i.e, whatever logic we want to perform)
   const router = express.Router();
 
+  // router.options("/", cors());
   router.get("/", controller.songsOnTableSongs); // calls the actual logic from the controller/songs/songs.js file
   router.post("/", controller.addSongsToTableSongs); // logic from controller/songs/postsongs.js
   router.post("/add", controller.addSongsToQueue); // logic from controller/songs/posttoqueue.js
