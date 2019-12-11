c4539403bce783a011c2953fbbf82c6f694764b8
Marco Valero <marcovalero6@gmail.com>
2019-12-11 12:10:59 -0500

    cors bug

--- routes/get-lobbies.js
+++ routes/get-lobbies.js
@@ -1,14 +1,14 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const { lobbies } = require("../controllers"); // gets lobbies directory from the index.js under controllers directory
 
 const setup = () => {
   const controller = lobbies(); // imports the controllers (i.e, whatever logic we want to perform)
   const router = express.Router();
 
-  // router.options("/", cors());
-  router.get("/", controller.lobbieslist);
-  router.post("/", controller.addNewLobby);
+  router.options("/", cors());
+  router.get("/", cors(), controller.lobbieslist);
+  router.post("/", cors(), controller.addNewLobby);
 
   return router;
 };
--- routes/get-songs.js
+++ routes/get-songs.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const { songs } = require("../controllers"); // the syntax "{ songs }" is the destructuring assignment syntax. This is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects,into distinct variables.
 // in order to be able to use the controllers by importing from the controller directory, the "songs"  variable has to match the key under the index.js file under controllers directory
 
@@ -7,13 +7,13 @@ const setup = () => {
   const controller = songs(); // imports the controllers (i.e, whatever logic we want to perform)
   const router = express.Router();
 
-  // router.options("/", cors());
-  router.get("/", controller.songsOnTableSongs); // calls the actual logic from the controller/songs/songs.js file
-  router.post("/", controller.addSongsToTableSongs); // logic from controller/songs/postsongs.js
-  router.post("/add", controller.addSongsToQueue); // logic from controller/songs/posttoqueue.js
-  router.get("/queue/:lobbycode", controller.getSongsInQueue); // logic from controller/songs/getsongsinqueue.js
-  router.delete("/delete", controller.delSongInQueue); // logic from controller/songs/deletesonginqueue.js
-  router.put("/rate/:lobbycode/:songcode/:rate", controller.updateSongRating);
+  router.options("/", cors());
+  router.get("/", cors(), controller.songsOnTableSongs); // calls the actual logic from the controller/songs/songs.js file
+  router.post("/", cors(), controller.addSongsToTableSongs); // logic from controller/songs/postsongs.js
+  router.post("/add", cors(), controller.addSongsToQueue); // logic from controller/songs/posttoqueue.js
+  router.get("/queue/:lobbycode", cors(), controller.getSongsInQueue); // logic from controller/songs/getsongsinqueue.js
+  router.delete("/delete", cors(), controller.delSongInQueue); // logic from controller/songs/deletesonginqueue.js
+  router.put("/rate/:lobbycode/:songcode/:rate", cors(), controller.updateSongRating);
 
   return router;
 };
